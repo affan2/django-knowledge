e917309c638dd9adf015a0fff39af9be5a8a42d6
Mohammed Affan <mohammed@thebimhub.com>
2014-07-02 10:29:46 +0400

    view updates for category questions

--- knowledge/views.py
+++ knowledge/views.py
@@ -36,23 +36,28 @@ def get_my_questions(request):
                                .filter(user=request.user)
 
 
+def get_categories(request):
+    categories = Category.objects.all()
+    for category in categories:
+        setattr(category, 'questions', Question.objects.can_view(request.user).filter(categories=category).prefetch_related('responses__question')[0:20])
+
+    return categories
+
+
 def knowledge_index(request,
                     template='django_knowledge/index.html'):
 
     if settings.LOGIN_REQUIRED and not request.user.is_authenticated():
         return HttpResponseRedirect(settings.LOGIN_URL+"?next=%s" % request.path)
 
-    questions = Question.objects.can_view(request.user)\
-                                .prefetch_related('responses__question')[0:20]
-    # this is for get_responses()
-    [setattr(q, '_requesting_user', request.user) for q in questions]
+    categories = get_categories(request)
 
     return render(request, template, {
         'request': request,
-        'questions': questions,
         'my_questions': get_my_questions(request),
-        'categories': Category.objects.all(),
-        'BASE_TEMPLATE' : settings.BASE_TEMPLATE,
+        'categories': categories,
+        'no_sidebar': True,
+        'BASE_TEMPLATE': settings.BASE_TEMPLATE,
     })
 
 
@@ -84,15 +89,17 @@ def knowledge_list(request,
     # this is for get_responses()
     [setattr(q, '_requesting_user', request.user) for q in questions]
 
+    categories = get_categories(request)
+
     return render(request, template, {
         'request': request,
         'search': search,
         'questions': questions,
         'my_questions': get_my_questions(request),
         'category': category,
-        'categories': Category.objects.all(),
+        'categories': categories,
         'form': Form(request.user, initial={'title': search}),  # prefill title
-        'BASE_TEMPLATE' : settings.BASE_TEMPLATE,
+        'BASE_TEMPLATE': settings.BASE_TEMPLATE,
     })
 
 
@@ -129,6 +136,8 @@ def knowledge_thread(request,
     else:
         form = Form(request.user, question)
 
+    categories = get_categories(request)
+
     return render(request, template, {
         'request': request,
         'question': question,
@@ -136,8 +145,8 @@ def knowledge_thread(request,
         'responses': responses,
         'allowed_mods': ALLOWED_MODS,
         'form': form,
-        'categories': Category.objects.all(),
-        'BASE_TEMPLATE' : settings.BASE_TEMPLATE,
+        'categories': categories,
+        'BASE_TEMPLATE': settings.BASE_TEMPLATE,
     })
 
 
@@ -215,10 +224,12 @@ def knowledge_ask(request,
     else:
         form = Form(request.user)
 
+    categories = get_categories(request)
+
     return render(request, template, {
         'request': request,
         'my_questions': get_my_questions(request),
         'form': form,
-        'categories': Category.objects.all(),
-        'BASE_TEMPLATE' : settings.BASE_TEMPLATE,
+        'categories': categories,
+        'BASE_TEMPLATE': settings.BASE_TEMPLATE,
     })
