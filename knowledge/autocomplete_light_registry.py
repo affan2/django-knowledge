a2448b1d4bac33bd11ba441380a5a88124104eec
Mohammed Affan <affan2@gmail.com>
2015-04-20 18:01:24 +0400

    added ordering and split words

--- knowledge/autocomplete_light_registry.py
+++ knowledge/autocomplete_light_registry.py
@@ -7,6 +7,8 @@ class QuestionAutocomplete(autocomplete_light.AutocompleteModelBase):
     search_fields = ['title']
     autocomplete_js_attributes = {'placeholder': 'Select a question', }
     choices = Question.objects.all()
+    order_by = ['title']
+    split_words = True
 
 autocomplete_light.register(Question, QuestionAutocomplete)
 
